1c2a8e50fd5a31987ea9c2e79a123fa07a3ee641
Alexander Mikhalchenko <mikhalchenko.alexander@gmail.com>
2017-04-05 19:56:15 +0300

    Add index page.

---
+++ mysite/mysite/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('mysite/templates/index.html')
+    print(loader)
+    return HttpResponse(template.render(request))
