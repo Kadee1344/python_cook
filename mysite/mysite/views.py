7938e1f1dc5a0410079be0a6a959c20c7e9504f6
aam <aam@anahoret.com>
2017-04-06 13:15:04 +0300

    Add static resources resolving.

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     template = loader.get_template('mysite/templates/index.html')
     print(loader)
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render(None, request))
